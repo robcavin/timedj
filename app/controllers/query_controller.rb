528be193de50e33513cef500ad81833979008ae0
Rob <rob@bumbleejuice.com>
2010-02-24 00:19:26 -0800

    Updated CSS tags to fix autocomlete.  Added link for GO button.  Right now it just displays a table for the city and country of the first two inputs separated by AND.

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -6,5 +6,39 @@ class QueryController < ApplicationController
      @cities = City.find(:all)
      @countries = Country.find(:all)   
   end
-  
-end
+
+    # GET new_message_url
+    def new
+      # return an HTML form for describing a new message
+    end
+
+    # POST messages_url
+    def create
+      # create a new message
+    end
+
+    # GET message_url(:id => 1)
+    def show
+      temp = params[:city][:name].split(" AND ");
+      @myinputs = [];
+      temp.each do |i|
+        @myinputs.push(City.find(:first,:conditions =>"name = '#{i}'"))
+      end
+      # find and return a specific message
+    end
+
+    # GET edit_message_url(:id => 1)
+    def edit
+      # return an HTML form for editing a specific message
+    end
+
+    # PUT message_url(:id => 1)
+    def update
+      # find and update a specific message
+    end
+
+    # DELETE message_url(:id => 1)
+    def destroy
+      # delete a specific message
+    end
+ end
